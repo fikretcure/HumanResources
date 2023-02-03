963e74a112d45f9b477cb333a2a3dae209cd9949
fikretcure <fikretcure@gmail.com>
2023-02-03 22:55:41 +0300

    Updated RouteNameEnum

---
+++ app/Enums/RouteNameEnum.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Support\Facades\Route;
+
+/**
+ *
+ */
+enum RouteNameEnum: string
+{
+    case users = "Kullanıcı";
+
+    case index = "Listeleme";
+    case store = "Ekleme";
+    case show = "Gösterme";
+    case update = "Güncelleme";
+    case destroy = "Silme";
+
+
+    /**
+     * @return mixed
+     */
+    public static function generateInfoMes(): mixed
+    {
+        return str()->of(Route::currentRouteName())->explode('.')->map(function ($name, $key) {
+            return (collect(self::cases())->map(function ($item, $key) use ($name) {
+                return $item->name == $name ? $item->value : false;
+            })->filter())->first();
+        })->implode(' ');
+    }
+
+}
