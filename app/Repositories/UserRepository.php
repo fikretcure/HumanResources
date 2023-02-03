ed34fdd4cf51f5987012226a7364bf4e26fae1d6
fikretcure <fikretcure@gmail.com>
2023-02-04 01:32:00 +0300

    Added

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repositories;
+
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+
+/**
+ *
+ */
+class UserRepository
+{
+    /**
+     * @var Builder
+     */
+    private Builder $model;
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->model = User::query();
+    }
+
+    /**
+     * @return array|Collection
+     */
+    public function index(): array|Collection
+    {
+        return $this->model->get();
+    }
+
+    /**
+     * @param array $attributes
+     * @return Builder|Model
+     */
+    public function store(array $attributes): Builder|Model
+    {
+        return $this->model->create(
+            attributes: ["password" => rand(), "reg_code" => rand()] + $attributes
+        );
+    }
+
+
+}
