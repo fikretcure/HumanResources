68517e7d7d7fa177c80b71447ccc7197b23490c2
fikretcure <fikretcure@gmail.com>
2023-02-04 17:52:50 +0300

    Added show in PasswordResetRepository

--- app/Repositories/PasswordResetRepository.php
+++ app/Repositories/PasswordResetRepository.php
@@ -4,9 +4,12 @@ namespace App\Repositories;
 
 
 use App\Models\PasswordReset;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Throwable;
 
 
 /**
@@ -44,6 +47,19 @@ class PasswordResetRepository extends Repository
         return $this->model->create(
             attributes: $attributes
         );
+    }
+
+    /**
+     * @param $uuid
+     * @return Builder|Model
+     * @throws Throwable
+     */
+    public function show($uuid): Model|Builder
+    {
+        $data = $this->model->where("token", $uuid)->firstOrFail();
+        throw_if(Carbon::now()->diffInMinutes($data->created_at) >= 5, Exception::class, 'Şifre değiştirme token geçerliliği bitmiştir');
+
+        return $data;
     }
 
 
