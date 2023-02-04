fc8f070263d2943c95502bd2e66fe777be4d62ef
fikretcure <fikretcure@gmail.com>
2023-02-04 19:09:11 +0300

    Upd userStoreMailJob.blade.php

--- resources/views/mail/userStoreMailJob.blade.php
+++ resources/views/mail/userStoreMailJob.blade.php
@@ -4,5 +4,7 @@
 
 
 <div>
-Sistemimizi kullanabilmen için , {{env("web_url")}}/password-reset/{{$token}} adresinden şifreni oluşturabilirsin.
+    Hesabınız şirketiniz tarafından oluşturuldu.Sistemimizi kullanabilmeniz için ,
+    {{env("web_url")}}/password-reset/{{$token}} adresinden şifre oluşturabilirsin.
+    Linkin güvenlik süresi geçmiş işe şifremi unuttum yönergelerini takip ederek yeni şifre oluşturabilirsin.
 </div>
