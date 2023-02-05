315830deb71e55c2cbf6ef95d3e6f4202c6b51ac
fikretcure <fikretcure@gmail.com>
2023-02-05 14:11:47 +0300

    Added AuthLoginRequest

---
+++ app/Http/Requests/AuthLoginRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class AuthLoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            "email" => [
+                "required",
+                "string",
+                "email:rfc,dns"
+            ],
+            "password" => [
+                'required',
+                "string",
+                Password::min(6)
+                    ->mixedCase()
+                    ->letters()
+                    ->numbers()
+                    ->symbols()
+            ],
+        ];
+    }
+}
