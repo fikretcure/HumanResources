119479f196b8754403c6091ac23c871280a57dd0
fikretcure <fikretcure@gmail.com>
2023-02-05 21:13:23 +0300

    Updated handle in AuthenticationMiddleware

--- app/Http/Middleware/AuthenticationMiddleware.php
+++ app/Http/Middleware/AuthenticationMiddleware.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use App\Helpers\RequestMerge;
+use App\Models\Token;
 use App\Traits\Responsed;
 use Closure;
 use Illuminate\Http\JsonResponse;
@@ -20,13 +22,34 @@ class AuthenticationMiddleware
      * @param Closure $next
      * @return JsonResponse
      */
-    public function handle(Request $request, Closure $next): JsonResponse
+    public function handle(Request $request, Closure $next): JsonResponse//: JsonResponse
     {
         if (env("APP_ENV") == "local") {
             Auth::loginUsingId(1);
             return $next($request);
         }
 
+        $token = Token::where("bearrer", $request->header("bearrer"))
+            ->where("refresh", $request->header("refresh"))
+            ->firstOrFail();
+
+
+        if (now()->lessThanOrEqualTo($token->bearrer_expired_at)) {
+            (new RequestMerge())->addToken($request->header("bearrer"), $request->header("refresh"));
+
+            return $next($request);
+        }
+
+        if (now()->lessThanOrEqualTo($token->refresh_expired_at)) {
+            $bearrer = str()->uuid();
+            (new RequestMerge())->addToken($bearrer, $request->header("refresh"));
+            $token->update([
+                "bearrer" => $bearrer,
+                "bearrer_expired_at" => now()->addMinutes(5),
+            ]);
+
+            return $next($request);
+        }
         return $this->failMes("Token Geçersiz")->send(403);
     }
 }
