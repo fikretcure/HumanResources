5bee0673bb513adb6b1dcd2ee7bd1cd3deb242fc
fikretcure <fikretcure@gmail.com>
2023-02-05 13:46:17 +0300

    Added token

---
+++ app/Models/Token.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Token extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "user_id",
+        "bearrer",
+        "bearrer_expired_at",
+        "refresh",
+        "refresh_expired_at"
+    ];
+}
