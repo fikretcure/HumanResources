fd970a1f1fd999e54f4ef49ca7373bc8bfbdd7ce
fikretcure <fikretcure@gmail.com>
2023-02-09 12:49:30 +0300

    Added Unit model

---
+++ app/Models/Unit.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Unit extends Model
+{
+    use HasFactory , SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'reg_code',
+        "parent_id"
+    ];
+
+    /**
+     * @var string
+     */
+    public static string $reg_code = "B";
+}
