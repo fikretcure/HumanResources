c044c6e57fffbddf6ba82fa0869100359b4349d9
fikretcure <fikretcure@gmail.com>
2023-02-10 01:52:16 +0300

    Updated __invoke in EndPointController

--- app/Http/Controllers/EndPointController.php
+++ app/Http/Controllers/EndPointController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Enums\RouteNameEnum;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Route;
-use App\Http\Middleware\AuthenticationMiddleware;
 
 /**
  *
@@ -18,18 +16,6 @@ class EndPointController extends Controller
      */
     public function __invoke(): JsonResponse
     {
-
-        $end_points = collect(Route::getRoutes()->get())->map(function ($item, $key) {
-            if (collect($item->action)->has("middleware")) {
-                if (collect($item->action["middleware"])->contains(AuthenticationMiddleware::class)) {
-                    return [
-                        "slug" => $item->action["as"],
-                        "name" => RouteNameEnum::generateInfoMes($item->action["as"])
-                    ];
-                }
-            }
-            return null;
-        })->filter()->values();
-        return $this->success($end_points)->send();
+        return $this->success(RouteNameEnum::getRouteNames())->send();
     }
 }
