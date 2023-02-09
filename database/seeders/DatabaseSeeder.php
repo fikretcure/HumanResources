0c0532301f72b6f07b7a2bd737c03dc6e52dd901
fikretcure <fikretcure@gmail.com>
2023-02-09 15:10:41 +0300

    Updated run in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            UnitSeeder::class
+            UnitSeeder::class,
+            UnitEndPointsSeeder::class
         ]);
     }
 }
