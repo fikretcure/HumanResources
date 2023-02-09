98e7e8fde24079053b1c65f2edb2fb8329a2ecf6
fikretcure <fikretcure@gmail.com>
2023-02-09 11:25:40 +0300

    Added EndPointController

---
+++ app/Http/Controllers/EndPointController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\RouteNameEnum;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\AuthenticationMiddleware;
+
+/**
+ *
+ */
+class EndPointController extends Controller
+{
+
+    /**
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+
+        $end_points = collect(Route::getRoutes()->get())->map(function ($item, $key) {
+            if (collect($item->action)->has("middleware")) {
+                if (collect($item->action["middleware"])->contains(AuthenticationMiddleware::class)) {
+                    return [
+                        "slug" => $item->action["as"],
+                        "name" => RouteNameEnum::generateInfoMes($item->action["as"])
+                    ];
+                }
+            }
+            return null;
+        })->filter()->values();
+        return $this->success($end_points)->send();
+    }
+}
