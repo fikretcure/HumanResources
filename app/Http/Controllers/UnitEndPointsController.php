c348ab0c6a55dac4279009c477bfc86cb6fb4d84
fikretcure <fikretcure@gmail.com>
2023-02-09 15:36:42 +0300

    Updated UnitEndpointRepository and UnitEndPointsController

--- app/Http/Controllers/UnitEndPointsController.php
+++ app/Http/Controllers/UnitEndPointsController.php
@@ -2,9 +2,78 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\UnitStoreRequest;
+use App\Http\Requests\UnitUpdateRequest;
+use App\Repositories\UnitEndpointRepository;
+use Illuminate\Http\JsonResponse;
+use Throwable;
 
+/**
+ *
+ */
 class UnitEndPointsController extends Controller
 {
-    //
+
+    /**
+     * @var UnitEndpointRepository
+     */
+    private UnitEndpointRepository $baseRepository;
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->baseRepository = new UnitEndpointRepository;
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        return $this->success($this->baseRepository->index())->send();
+    }
+
+    /**
+     * @param UnitStoreRequest $request
+     * @return JsonResponse
+     */
+    public function store(UnitStoreRequest $request): JsonResponse
+    {
+        return $this->success($this->baseRepository->store($request->validated()))->send();
+    }
+
+    /**
+     * @param $id
+     * @return JsonResponse
+     */
+    public function show($id): JsonResponse
+    {
+        return $this->success($this->baseRepository->show($id))->send();
+    }
+
+    /**
+     * @param UnitUpdateRequest $request
+     * @param $id
+     * @return JsonResponse
+     * @throws Throwable
+     */
+    public function update(UnitUpdateRequest $request, $id): JsonResponse
+    {
+        $this->baseRepository->update($request->validated(), $id);
+
+        return $this->success()->send();
+    }
+
+    /**
+     * @param $id
+     * @return JsonResponse
+     * @throws Throwable
+     */
+    public function destroy($id): JsonResponse
+    {
+        return $this->success($this->baseRepository->destroy($id))->send();
+    }
+
 }
---
+++ app/Repositories/UnitEndpointRepository.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Repositories;
+
+
+use App\Models\UnitEndPoints;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+
+/**
+ *
+ */
+class UnitEndpointRepository extends Repository
+{
+    /**
+     * @var Builder
+     */
+    private Builder $model;
+
+    /**
+     * @var string
+     */
+    private $model_class;
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->model = UnitEndPoints::query();
+        $this->model_class = UnitEndPoints::class;
+    }
+
+    /**
+     * @return array|Collection
+     */
+    public function index(): array|Collection
+    {
+        return $this->model->get();
+    }
+
+    /**
+     * @param array $attributes
+     * @return Model|Builder
+     */
+    public function store(array $attributes): Builder|Model
+    {
+        return $this->model->create(
+            attributes: ["reg_code" => $this->generateRegCode($this->model_class)] + $attributes
+        );
+    }
+
+    /**
+     * @param array $attributes
+     * @param int $id
+     * @return bool|int
+     */
+    public function update(array $attributes, int $id): bool|int
+    {
+        return $this->model->findOrFail($id)->update($attributes);
+    }
+
+    /**
+     * @param int $id
+     * @return Model|Collection|Builder|array
+     */
+    public function show(int $id): Model|Collection|Builder|array
+    {
+        return $this->model->findOrFail($id);
+    }
+
+    /**
+     * @param int $id
+     * @return bool|mixed|null
+     */
+    public function destroy(int $id): mixed
+    {
+        return $this->model->findOrFail($id)->delete();
+    }
+
+}
