ead506f33f37746864b2aab0122e56a0d743d827
fikretcure <fikretcure@gmail.com>
2023-05-07 20:41:07 +0300

    Added web.php

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/setup', function () {
+Route::post('/setup', function () {
     $result = Process::run('cd .. && bash import.sh');
     return $result->output();
 });
