6207d3053e7c6290bd48cbdce799c0c23468ca3c
fikretcure <fikretcure@gmail.com>
2023-05-07 13:08:56 +0300

    Updated Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-      $schedule->command('backup:run')->everyFiveMinutes();
+      $schedule->command('backup:run')->hourly();
     }
 
     /**
