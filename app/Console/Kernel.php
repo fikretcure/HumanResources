7ffe7f4cdaaf054b6fabe44414b084dc78d5c0d8
fikretcure <fikretcure@gmail.com>
2023-05-09 23:12:15 +0300

    first commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
       $schedule->command('backup:run')->daily();
+      $schedule->command('queue:listen')->hourly();
     }
 
     /**
