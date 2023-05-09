a16353499f27d19d99d8ccb44976f2a92c2f70c2
fikretcure <fikretcure@gmail.com>
2023-05-09 19:11:21 +0300

    first commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,18 +11,21 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
 
-
     public function login(LoginAuthRequest $request)
     {
-        if (Auth::attempt($request->only('email','password'))) {
+        if (Auth::attempt($request->only('email', 'password'))) {
             return $request->user()->createToken($request->device)->plainTextToken;
         }
         return response()->json('Giris bilgilerinizi kontrol etmelisiniz !', 403);
     }
 
-
     public function auth()
     {
         return \auth()->user();
+    }
+
+    public function logout()
+    {
+        return request()->user()->tokens()->delete();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('auth', [AuthController::class, 'auth']);
+    Route::name("auth.")->controller(AuthController::class)->group(function () {
+        Route::get('auth', [AuthController::class, 'auth']);
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
 });
 
 Route::post('setup', SetupController::class);
