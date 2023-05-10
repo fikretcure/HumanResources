81744138d6676b64b8748a216973cca7963bf83d
fikretcure <fikretcure@gmail.com>
2023-05-10 22:08:02 +0300

    first commit

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::name("auth.")->controller(AuthController::class)->group(function () {
-        Route::get('auth', [AuthController::class, 'auth']);
+        Route::get('auth', [AuthController::class, 'auth'])->name('show');
         Route::post('logout', [AuthController::class, 'logout'])->name('logout');
     });
 });
