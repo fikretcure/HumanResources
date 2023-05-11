6837ca5e6abfd7beb2899c489fab16bd1ff6ddb2
fikretcure <79414903+fikretcure@users.noreply.github.com>
2023-05-11 16:47:17 +0300

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,12 +4,10 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
-use App\Traits\ResponseTrait;
-
+ 
 class Handler extends ExceptionHandler
 {
-    use ResponseTrait
-    
+     
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -26,18 +24,6 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
-        $this->renderable(function (Throwable $exception, $request) {
-            if ($exception instanceof ValidationException) {
-                return $this->fail($exception->validator->getMessageBag())->mes("validation")->send(402);
-            }
-
-            if ($exception->getPrevious() instanceof RecordsNotFoundException) {
-                return $this->fail($exception->getMessage())->mes('İşlemini yapmak istediğiniz kayıt malesef bulunamadı.Yolunda gitmeyen bir şeyler var !')->send();
-            }
-
-            return $this->fail($exception->getMessage())->mes($exception->getMessage())->send(402);
-        });
-
         $this->reportable(function (Throwable $e) {
             //
         });
