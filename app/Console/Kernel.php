33eb50965fabbc09b51980371e04be3678e3d437
fikretcure <fikretcure@gmail.com>
2023-05-12 06:14:09 +0300

    Updated AuthController.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-      $schedule->command('backup:run')->daily();
+      $schedule->command('backup:run')->hourly();
       $schedule->command('queue:listen')->everyMinute();
     }
 
