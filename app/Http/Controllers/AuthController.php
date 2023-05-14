fe35c71409fb6e8fad10a9ee1df24d68a145e128
Fikret CURE <fikretcure@gmail.com>
2023-05-14 18:17:53 +0300

    Updated AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginAuthRequest;
-use App\Jobs\LoginJob;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Throwable;
@@ -21,15 +20,10 @@ class AuthController extends Controller
      */
     public function login(LoginAuthRequest $request): JsonResponse
     {
-        try {
             if (Auth::attempt($request->only('email', 'password'))) {
-                LoginJob::dispatch();
                 return $this->success($request->user()->createToken($request->device)->plainTextToken)->send();
             }
             return $this->failMes('Kullanici bilgilerinizi kontrol etmelisiniz')->send();
-        } catch (Throwable $exception) {
-            return $this->failMes($exception->getPrevious())->send(403);
-        }
     }
 
     /**
