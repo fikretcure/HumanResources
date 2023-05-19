f8dcf280b7175f66bd52750d4e49c91d6eb413e8
Fikret CURE <fikretcure@gmail.com>
2023-05-19 18:16:36 +0300

    Updated DepartmentRepository.php

--- app/Repositories/DepartmentRepository.php
+++ app/Repositories/DepartmentRepository.php
@@ -3,15 +3,34 @@
 namespace App\Repositories;
 
 use App\Models\Department;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ *
+ */
 class DepartmentRepository extends Repository
 {
+    /**
+     * @var Model
+     */
     public Model $model;
 
+    /**
+     *
+     */
     public function __construct()
     {
         $this->model = new Department();
         parent::__construct($this->model);
+    }
+
+    /**
+     * @return Builder[]|Collection
+     */
+    public function detail(): Collection|array
+    {
+        return $this->model->with('positions')->get();
     }
 }
