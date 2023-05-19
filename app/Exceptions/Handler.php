c0e28513ad731e3a8f1b1cdbf63f30079cf456e8
Fikret CURE <fikretcure@gmail.com>
2023-05-19 11:42:48 +0300

    Updated Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,6 +6,7 @@ use App\Traits\ResponseTrait;
 use Illuminate\Database\RecordsNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
+use Spatie\Permission\Exceptions\UnauthorizedException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -23,20 +24,31 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
+
     /**
      * Register the exception handling callbacks for the application.
      */
     public function register(): void
     {
         $this->renderable(function (Throwable $exception) {
+            $fail = $exception->getMessage();
+            $code = 404;
+            $mes = $exception->getTrace();
+
             if ($exception instanceof ValidationException) {
-                return $this->failMes($exception->validator->getMessageBag())->send(402);
+                $fail = $exception->validator->getMessageBag();
+                $mes = 'Girmis oldugunuz verilerini kontrol etmelisiniz';
+                $code = 402;
+            }
+            if ($exception instanceof RecordsNotFoundException) {
+                $mes = 'Istenilen kayit bulunamamistir';
+            }
+            if ($exception instanceof UnauthorizedException) {
+                $mes = 'Islem icin yetkisizsiniz';
+                $code = 403;
             }
 
-            if ($exception->getPrevious() instanceof RecordsNotFoundException) {
-                return $this->fail($exception->getMessage())->mes('Istenilen kayit bulunamamistir')->send();
-            }
-            return $this->fail($exception->getTrace())->mes($exception->getMessage())->send();
+            return $this->fail($fail)->mes($mes)->send($code);
         });
 
         $this->reportable(function (Throwable $e) {
