6360684c286834657f60546cec9470c8167a5ca7
Fikret CURE <fikretcure@gmail.com>
2023-05-19 18:28:12 +0300

    Updated Position.php

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Position extends Model
 {
@@ -19,4 +22,12 @@ class Position extends Model
         'parent_id',
         'department_id',
     ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
