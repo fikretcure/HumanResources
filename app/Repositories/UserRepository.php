e6a1230159b49863b62726d0bf47510733b7b28b
Fikret CURE <fikretcure@gmail.com>
2023-05-21 18:54:28 +0300

    Updated UserRepository.php

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -5,13 +5,31 @@ namespace App\Repositories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ *
+ */
 class UserRepository extends Repository
 {
-    public Model $model;
+    /**
+     * @var Model|User
+     */
+    public Model|User $model;
 
+    /**
+     *
+     */
     public function __construct()
     {
         $this->model = new User();
         parent::__construct($this->model);
+    }
+
+    /**
+     * @param $email
+     * @return mixed
+     */
+    public function getUserByEmail($email)
+    {
+        return $this->model->firstWhere('email', $email);
     }
 }
