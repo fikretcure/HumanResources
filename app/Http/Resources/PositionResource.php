3802b2637b9125d96e4fa77fefd87ef827ebd0aa
Fikret CURE <fikretcure@gmail.com>
2023-05-27 14:13:54 +0300

    Updated PositionResource.php

--- app/Http/Resources/PositionResource.php
+++ app/Http/Resources/PositionResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Department;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -17,6 +18,7 @@ class PositionResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'department' => $this->department,
             'created_at' => $this->created_at,
         ];
     }
