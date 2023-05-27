939f73ba5170ecd08ca1ee32e292aa23c874e79f
Fikret CURE <fikretcure@gmail.com>
2023-05-27 19:23:52 +0300

    Added 2023_03_20_044016_create_tokens_table.php

---
+++ database/migrations/2023_03_20_044016_create_tokens_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->id();
+            $table->string('token');
+            $table->string('type');
+            $table->string('remote_addr')->nullable();
+            $table->string('server_addr')->nullable();
+            $table->string('http_user_agent')->nullable();
+            $table->json('location')->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tokens');
+    }
+};
