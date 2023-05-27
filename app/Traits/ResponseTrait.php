2cbd53d495ae73e85201aa959c78f0e7d115ca30
Fikret CURE <fikretcure@gmail.com>
2023-05-27 15:26:34 +0300

    Updated ResponseTrait.php

--- app/Traits/ResponseTrait.php
+++ app/Traits/ResponseTrait.php
@@ -14,22 +14,29 @@ trait ResponseTrait
 {
 
     /**
-     * @param $data
-     * @return JsonResponse
+     * @param bool $status
+     * @return array
      */
-    public function okPaginate($data = null): JsonResponse
+    public function generateHistoryData(bool $status = true): array
     {
-        $history_data = [
+        return [
             'data' => json_encode([
                 'route' => RouteName::statusNote(),
                 'request' => request()->except('password'),
                 'url' => request()->url()
             ]),
             'user_id' => Auth::check() ? Auth::id() : 0,
-            'status' => 1
+            'status' => $status
         ];
+    }
 
-        CreateHistoryJob::dispatch($history_data);
+    /**
+     * @param $data
+     * @return JsonResponse
+     */
+    public function okPaginate($data = null): JsonResponse
+    {
+        CreateHistoryJob::dispatch($this->generateHistoryData());
         $data = $data->response()->getData(true);
         return response()->json([
             "information" => [RouteName::statusNote() . " " . "Basarili"],
@@ -45,17 +52,7 @@ trait ResponseTrait
      */
     public function ok($data = null): JsonResponse
     {
-        $history_data = [
-            'data' => json_encode([
-                'route' => RouteName::statusNote(),
-                'request' => request()->except('password'),
-                'url' => request()->url()
-            ]),
-            'user_id' => Auth::check() ? Auth::id() : 0,
-            'status' => 1
-        ];
-
-        CreateHistoryJob::dispatch($history_data);
+        CreateHistoryJob::dispatch($this->generateHistoryData());
         return response()->json([
             "information" => [RouteName::statusNote() . " " . "Basarili"],
             "data" => $data,
@@ -70,19 +67,7 @@ trait ResponseTrait
      */
     public function error(string $note = null, $fail = null, int $status_code = null): JsonResponse
     {
-        $history_data = [
-            'data' => json_encode([
-                'route' => RouteName::statusNote(),
-                'request' => request()->except('password'),
-                'url' => request()->url()
-            ]),
-            'user_id' => Auth::check() ? Auth::id() : 0,
-            'status' => 0
-        ];
-
-        CreateHistoryJob::dispatch($history_data);
-
-
+        CreateHistoryJob::dispatch($this->generateHistoryData(false));
         return response()->json([
             "information" => [RouteName::statusNote() . " " . "Basarisiz", $note],
             "fail" => $fail
