cadc7e0f3f4da1869f8e41acec38e605888a391a
Fikret CURE <fikretcure@gmail.com>
2023-05-27 09:41:02 +0300

    Updated SetupController.php

--- app/Http/Controllers/SetupController.php
+++ app/Http/Controllers/SetupController.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Process;
 
 /**
@@ -22,6 +23,7 @@ class SetupController extends Controller
     {
         if (env('APP_DEVICE') == 'development' or request()->bearerToken() == env('APP_KEY')) {
             $result = Process::run('cd .. && php artisan migrate:fresh --seed --force');
+            DB::commit();
             return response($result->output());
         }
         return $this->error();
