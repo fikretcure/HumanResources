46f451f4009a1ba5e5ba9ae83dcc08c4b3c4ef91
Fikret CURE <fikretcure@gmail.com>
2023-05-28 15:32:18 +0300

    Updated shipped.blade.php

--- resources/views/emails/authlogin/shipped.blade.php
+++ resources/views/emails/authlogin/shipped.blade.php
@@ -1,6 +1,12 @@
 <x-mail::message>
     # Sisteme Giris Yaptiniz
 
+    # Datetime {{$data['date']}}
+
+    # Remote Addr {{$data['remote_addr']}}
+    # Server Addr {{$data['server_addr']}}
+    # Browser {{$data['http_user_agent']}}
+
     Kolayliklar dileriz,Iyi calismalar<br>
     {{ config('app.name') }}
 </x-mail::message>
