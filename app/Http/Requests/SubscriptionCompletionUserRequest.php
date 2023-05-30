29d4e97933ff0dce2220b7d0a837951e8a72fac9
Fikret CURE <fikretcure@gmail.com>
2023-05-30 22:15:59 +0300

    Added SubscriptionCompletionUserRequest.php

---
+++ app/Http/Requests/SubscriptionCompletionUserRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\MembershipInvitations;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class SubscriptionCompletionUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'token' => [
+                'string',
+                'required',
+                Rule::exists(MembershipInvitations::class)
+            ]
+        ];
+    }
+}
