bb31df7eb1c73fbf45375e9a922f5459d7aabb86
Fikret CURE <fikretcure@gmail.com>
2023-05-30 22:37:17 +0300

    Upd 2023_05_30_133914_create_membership_invitations_table.php

--- database/migrations/2023_05_30_133914_create_membership_invitations_table.php
+++ database/migrations/2023_05_30_133914_create_membership_invitations_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('surname');
             $table->string('email');
+            $table->string('phone');
             $table->string('token');
             $table->dateTime('expired_at');
             $table->timestamps();
