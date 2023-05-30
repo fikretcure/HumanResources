589ca2ec50edd3044b8d3b38891448492bffa8bc
Fikret CURE <fikretcure@gmail.com>
2023-05-30 14:32:40 +0300

    Added shipped.blade.php

---
+++ resources/views/emails/membershipInvitations/shipped.blade.php
@@ -0,0 +1,9 @@
+<x-mail::message>
+# Uyelik Daveti
+
+Baglantiya tiklayarak sifrenizi belirlemelisiniz,ardindan sistemi kullanmaya baslayabilirsiniz
+
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
