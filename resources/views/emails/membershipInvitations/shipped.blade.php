1bfc44492976a3e5a7b95658a27fa4bbc4c51dd2
Fikret CURE <fikretcure@gmail.com>
2023-06-17 13:14:03 +0300

    Upd shipped.blade.php

--- resources/views/emails/membershipInvitations/shipped.blade.php
+++ resources/views/emails/membershipInvitations/shipped.blade.php
@@ -3,7 +3,7 @@
 
 Baglantiya tiklayarak sifrenizi belirleyip, sistemi kullanmaya baslayabilirsiniz
 
-{{env('FRONT_TEST_URL')}}/password-reset?token={{$data['token']}}
+{{env('FRONT_TEST_URL')}}/subscription-completion?token={{$data['token']}}
 
 Thanks,<br>
 {{ config('app.name') }}
