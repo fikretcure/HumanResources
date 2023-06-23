14ec37d2b7739fc3b73ccdfb942152aecf985148
Fikret CURE <fikretcure@gmail.com>
2023-06-23 21:34:39 +0300

    Added UserResource.php

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            "name" => $this->name,
+            "surname" => $this->surname,
+            "phone" => $this->phone,
+            "email" => $this->email,
+            "status" => $this->status,
+            "birth_at" => $this->birth_at,
+            "sex" => $this->sex,
+            "start_work" => $this->start_work,
+            "end_work" => $this->end_work,
+        ];
+    }
+}
