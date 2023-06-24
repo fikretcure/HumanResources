f69de01fc633cf36f22ab9f8b9327c1256b584aa
Fikret CURE <fikretcure@gmail.com>
2023-06-24 21:03:54 +0300

    Upd UpdateUserRequest.php

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Department;
+use App\Models\Position;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'string',
+            ]
+        ];
+    }
+}
