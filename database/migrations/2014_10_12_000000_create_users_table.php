23232afb6822f68c04e5060d73046eb4d76816e1
Fikret CURE <fikretcure@gmail.com>
2023-06-24 21:06:35 +0300

    Upd 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
             $table->id();
             $table->string('name');
             $table->string('surname');
-            $table->string('phone');
+            $table->string('phone')->unique();;
             $table->string('email')->unique();
             $table->string('password');
             $table->boolean('status');
